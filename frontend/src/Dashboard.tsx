8c0815d85a96bd516191db8cbbe9a3920aaa938d
Joe Do <joedo2910@gmail.com>
2026-01-31 18:48:29 -0500

    requirements.txt

---
+++ frontend/src/Dashboard.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function Dashboard() {
+  return (
+    <div className="min-h-screen bg-gray-50 text-gray-900 p-6">
+        <p> hi</p>
+    </div>
+  )
+}
+
+export default Dashboard
