1ff60a0c44c7c69fa67277300b39d57bb07bdf0e
Joe Do <joedo2910@gmail.com>
2026-01-31 18:08:38 -0500

    done

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -6,8 +6,14 @@ export default defineConfig({
     plugins: [tailwindcss()],
   }),
   manifest: {
-    name: 'Card Optimizer',
+    name: 'Dime',
     description: 'Get the best credit card recommendation at checkout',
+    icons: {
+      16: 'icon/16.png',
+      32: 'icon/32.png',
+      48: 'icon/48.png',
+      128: 'icon/128.png',
+    },
     permissions: ['storage', 'activeTab', 'tabs'],
     host_permissions: [
       // Shopping
